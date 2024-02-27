5fc5a3a7846c50b8f99386e3f8c6e694eee2a4c2
Eungsu Son <eungsu1217@gmail.com>
2024-02-27 20:01:17 +0900

    gg

--- js/main.js
+++ js/main.js
@@ -48,3 +48,30 @@ function Main(delay) {
   }
  
 Main(20000)
+
+const APIKEY = "f3ca5cbf-842e-439f-829e-45f6a648fca2";
+
+let getGreedIndex = async () => {
+    try {
+        const url = new URL("https://api.alternative.me/fng/");
+
+        const res = await fetch(url, {
+            headers: {
+                Accept: "application/json",
+                "X-CMC_PRO_API_KEY": APIKEY,
+            },
+        });
+        console.log(res);
+
+        if (!res.ok) {
+            const errorMessage = ` ${res.status}: ${res.statusText}`;
+            throw new Error(errorMessage);
+        }
+
+        const data = await res.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getGreedIndex();
