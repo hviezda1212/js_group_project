32f3f2534bfa77dcc4701ff49595359271bdc153
jebi2420 <jebi2420@gmail.com>
2024-02-28 20:19:19 +0900

    [init] getHotTop 함수 생성

--- js/main.js
+++ js/main.js
@@ -136,6 +136,17 @@ function Main(delay) {
 //20초마다 코인 정보를 업데이트한다
 Main(20000);
 
+// 가장 뜨거운 코인 top3
+const getHotTop = async () => {
+  try{
+    
+
+  }catch(error){
+
+  }
+
+}
+
 const APIKEY = "f3ca5cbf-842e-439f-829e-45f6a648fca2";
 
 let getGreedIndex = async () => {
