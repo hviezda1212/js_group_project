331260a4b3fa75d99f68aad1eb1aa15e7fe8be94
jebi2420 <jebi2420@gmail.com>
2024-03-01 20:53:38 +0900

    [add] api키 추가, 제목 css 수정

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // const apiKey = "b1b9c007-5f07-4d7c-b26f-948e542b8144";
 //const apiKey = "a44490f9-d234-41d8-86da-9a3dcef3ca5d";
-const apiKey = "f3ca5cbf-842e-439f-829e-45f6a648fca2";
+//const apiKey = "f3ca5cbf-842e-439f-829e-45f6a648fca2";
 const url =
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000";
 const mode = "no-cors";
