aaecf02fac06daf8106d01632e02693d5ddb97a8
jebi2420 <jebi2420@gmail.com>
2024-03-03 03:42:59 +0900

    [fix] rendering -> searchRender로 이름 변경 및 주석추가

--- js/main.js
+++ js/main.js
@@ -1,259 +1,260 @@
 // // const apiKey = "b1b9c007-5f07-4d7c-b26f-948e542b8144";
 // // const  apiKey= "a44490f9-d234-41d8-86da-9a3dcef3ca5d";
-// const url =
-//     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000";
-// const mode = "no-cors";
-// let coinList = [];
-// let prevPriceList = {};
-// let totalResult = 0;
-// let page = 1;
-// const pageSize = 100;
-// const groupSize = 5;
-// let currentPrice = 0;
+//const apiKey="80c825a1-4e89-4d63-b2db-81486a36d7f7";
+const url =
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000";
+const mode = "no-cors";
+let coinList = [];
+let prevPriceList = {};
+let totalResult = 0;
+let page = 1;
+const pageSize = 100;
+const groupSize = 5;
+let currentPrice = 0;
 
-// const initializePrevPriceList = async () => {
-//     coinList = [];
-//     console.log(url);
-//     const response = await fetch(url, {
-//         headers: {
-//             "X-CMC_PRO_API_KEY": apiKey,
-//             mode: mode,
-//         },
-//     });
-//     const data = await response.json();
-//     for (i = 0; i < data.data.length; i++) {
-//         tempCoinSymbol = data.data[i]["symbol"];
-//         prevPriceList[tempCoinSymbol] = [0];
-//         //   console.log(prevPriceList)
-//     }
-// };
-// initializePrevPriceList();
+const initializePrevPriceList = async () => {
+    coinList = [];
+    console.log(url);
+    const response = await fetch(url, {
+        headers: {
+            "X-CMC_PRO_API_KEY": apiKey,
+            mode: mode,
+        },
+    });
+    const data = await response.json();
+    for (i = 0; i < data.data.length; i++) {
+        tempCoinSymbol = data.data[i]["symbol"];
+        prevPriceList[tempCoinSymbol] = [0];
+        //   console.log(prevPriceList)
+    }
+};
+initializePrevPriceList();
 
-// const getData = async () => {
-//     try {
-//         coinList = [];
-//         console.log(coinList);
-//         // console.log(url);
-//         const response = await fetch(url, {
-//             headers: {
-//                 "X-CMC_PRO_API_KEY": apiKey,
-//                 mode: mode,
-//             },
-//         });
-//         const data = await response.json();
-//         //화면에 접속했을때 page =1이 기본값
-//         //for loop으로 100개씩돌려서 coinList에 push
-//         //2페이지를 눌렀을땐 page = 2가 되어서 101번째 코인부터 100개가 coinList로 들어간뒤 render()
-//         if (page === 1) {
-//             for (i = 0; i < pageSize; i++) {
-//                 coinList.push(data.data[i]);
-//                 // prevPriceList에 코인 가격 정보를 쌓는다
-//                 tempCoinSymbol = coinList[i]["symbol"];
-//                 tempCoinPrice = coinList[i]["quote"].USD["price"];
-//                 prevPriceList[tempCoinSymbol].push(tempCoinPrice);
-//             }
-//         } else {
-//             for (i = page * 100 - 99; i < pageSize * page + 1; i++) {
-//                 coinList.push(data.data[i]);
-//                 // prevPriceList에 코인 가격 정보를 쌓는다
-//                 tempCoinSymbol = coinList[i]["symbol"];
-//                 tempCoinPrice = coinList[i]["quote"].USD["price"];
-//                 prevPriceList[tempCoinSymbol].push(tempCoinPrice);
-//             }
-//         }
-//         totalResult = data.data.length;
-//         // console.log("ttt", coinList.length);
-//         render();
-//         paginationRender();
-//         // console.log("priceis", prevPriceList["BTC"][prevPriceList["BTC"].length-1])
-//         // console.log("prevpriceis", prevPriceList["BTC"][prevPriceList["BTC"].length-2])
-//     } catch (error) {
-//         console.log("error", error);
-//     }
-// };
+const getData = async () => {
+    try {
+        coinList = [];
+        console.log(coinList);
+        // console.log(url);
+        const response = await fetch(url, {
+            headers: {
+                "X-CMC_PRO_API_KEY": apiKey,
+                mode: mode,
+            },
+        });
+        const data = await response.json();
+        //화면에 접속했을때 page =1이 기본값
+        //for loop으로 100개씩돌려서 coinList에 push
+        //2페이지를 눌렀을땐 page = 2가 되어서 101번째 코인부터 100개가 coinList로 들어간뒤 render()
+        if (page === 1) {
+            for (i = 0; i < pageSize; i++) {
+                coinList.push(data.data[i]);
+                // prevPriceList에 코인 가격 정보를 쌓는다
+                tempCoinSymbol = coinList[i]["symbol"];
+                tempCoinPrice = coinList[i]["quote"].USD["price"];
+                prevPriceList[tempCoinSymbol].push(tempCoinPrice);
+            }
+        } else {
+            for (i = page * 100 - 99; i < pageSize * page + 1; i++) {
+                coinList.push(data.data[i]);
+                // prevPriceList에 코인 가격 정보를 쌓는다
+                tempCoinSymbol = coinList[i]["symbol"];
+                tempCoinPrice = coinList[i]["quote"].USD["price"];
+                prevPriceList[tempCoinSymbol].push(tempCoinPrice);
+            }
+        }
+        totalResult = data.data.length;
+        // console.log("ttt", coinList.length);
+        render();
+        paginationRender();
+        // console.log("priceis", prevPriceList["BTC"][prevPriceList["BTC"].length-1])
+        // console.log("prevpriceis", prevPriceList["BTC"][prevPriceList["BTC"].length-2])
+    } catch (error) {
+        console.log("error", error);
+    }
+};
 
-// const render = () => {
-//     let tableHTML = "";
-//     for (i = 0; i < coinList.length; i++) {
-//         coinSymbol = coinList[i]["symbol"];
-//         coinPrice = coinList[i]["quote"].USD["price"];
-//         tableHTML += `            <tr>
-//         <td id = "favorite"><button class = "fav-button"><i class="fa-regular fa-star"></i></button></td>
-//         <td id = "rank">${page === 1 ? i + 1 : page * 100 - 99 + i}</td>
-//         <td id = "name"><img class = "coin-img-size" src ='https://s2.coinmarketcap.com/static/img/coins/64x64/${
-//             coinList[i]["id"]
-//         }.png'></img><span>${coinList[i]["name"]}</span></td>
-//         <td id = "symbol">${coinList[i]["symbol"]}</td>
-//         <td id = "price">${checkPriceChange(coinPrice, coinSymbol)}</td>
-//         <td id = "1h">${
-//             coinList[i]["quote"].USD["percent_change_1h"].toFixed(2) + "%"
-//         }</td>
-//         <td id = "24h">${
-//             coinList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
-//         }</td>
-//         <td id = "7d">${
-//             coinList[i]["quote"].USD["percent_change_7d"].toFixed(2) + "%"
-//         }</td>
-//         <td id = "market-cap">${
-//             "$" +
-//             Math.floor(coinList[i]["quote"].USD["market_cap"]).toLocaleString()
-//         }</td>
-//         <td id = "volume">${
-//             "$" +
-//             Math.floor(coinList[i]["quote"].USD["volume_24h"]).toLocaleString()
-//         }</td>
-//         <td id = "circulating-supply">${
-//             Math.floor(coinList[i]["circulating_supply"]).toLocaleString() +
-//             " " +
-//             coinList[i]["symbol"]
-//         }</td>
-//     </tr>`;
-//         currentPrice = coinList[i]["quote"].USD["price"];
-//     }
-//     document.getElementById("table-data").innerHTML = tableHTML;
-//     // console.log(tableHTML);
-// };
+const render = () => {
+    let tableHTML = "";
+    for (i = 0; i < coinList.length; i++) {
+        coinSymbol = coinList[i]["symbol"];
+        coinPrice = coinList[i]["quote"].USD["price"];
+        tableHTML += `            <tr>
+        <td id = "favorite"><button class = "fav-button"><i class="fa-regular fa-star"></i></button></td>
+        <td id = "rank">${page === 1 ? i + 1 : page * 100 - 99 + i}</td>
+        <td id = "name"><img class = "coin-img-size" src ='https://s2.coinmarketcap.com/static/img/coins/64x64/${
+            coinList[i]["id"]
+        }.png'></img><span>${coinList[i]["name"]}</span></td>
+        <td id = "symbol">${coinList[i]["symbol"]}</td>
+        <td id = "price">${checkPriceChange(coinPrice, coinSymbol)}</td>
+        <td id = "1h">${
+            coinList[i]["quote"].USD["percent_change_1h"].toFixed(2) + "%"
+        }</td>
+        <td id = "24h">${
+            coinList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
+        }</td>
+        <td id = "7d">${
+            coinList[i]["quote"].USD["percent_change_7d"].toFixed(2) + "%"
+        }</td>
+        <td id = "market-cap">${
+            "$" +
+            Math.floor(coinList[i]["quote"].USD["market_cap"]).toLocaleString()
+        }</td>
+        <td id = "volume">${
+            "$" +
+            Math.floor(coinList[i]["quote"].USD["volume_24h"]).toLocaleString()
+        }</td>
+        <td id = "circulating-supply">${
+            Math.floor(coinList[i]["circulating_supply"]).toLocaleString() +
+            " " +
+            coinList[i]["symbol"]
+        }</td>
+    </tr>`;
+        currentPrice = coinList[i]["quote"].USD["price"];
+    }
+    document.getElementById("table-data").innerHTML = tableHTML;
+    // console.log(tableHTML);
+};
 
-// const checkPriceChange = (price, symbol) => {
-//     if (price > prevPriceList[symbol][prevPriceList[symbol].length - 2]) {
-//         // document.getElementById("price").className = "higher"
-//         return `<span style = "color:green">${
-//             "$" +
-//             price.toLocaleString("en-US", {
-//                 maximumFractionDigits: 2,
-//             })
-//         }</span>`;
-//         //초록색
-//     } else if (
-//         price < prevPriceList[symbol][prevPriceList[symbol].length - 2]
-//     ) {
-//         // document.getElementById("price").className = "lower"
-//         return `<span style = "color:red">${
-//             "$" +
-//             price.toLocaleString("en-US", {
-//                 maximumFractionDigits: 2,
-//             })
-//         }</span>`;
-//         //빨간색
-//     } else {
-//         // document.getElementById("price").className = "same"
-//         return `<span style = "color:black">${
-//             "$" +
-//             price.toLocaleString("en-US", {
-//                 maximumFractionDigits: 2,
-//             })
-//         }</span>`;
-//         //검은색
-//     }
-//     // prevPriceList의 가격정보를 불러와 가격을 비교한다
-//     // -1인덱스가격 (최신가격)이 -2인덱스가격 (전가격)보다 높으면 초록색, 낮으면 빨간색, 같으면 검은색으로 표시한다
-// };
+const checkPriceChange = (price, symbol) => {
+    if (price > prevPriceList[symbol][prevPriceList[symbol].length - 2]) {
+        // document.getElementById("price").className = "higher"
+        return `<span style = "color:green">${
+            "$" +
+            price.toLocaleString("en-US", {
+                maximumFractionDigits: 2,
+            })
+        }</span>`;
+        //초록색
+    } else if (
+        price < prevPriceList[symbol][prevPriceList[symbol].length - 2]
+    ) {
+        // document.getElementById("price").className = "lower"
+        return `<span style = "color:red">${
+            "$" +
+            price.toLocaleString("en-US", {
+                maximumFractionDigits: 2,
+            })
+        }</span>`;
+        //빨간색
+    } else {
+        // document.getElementById("price").className = "same"
+        return `<span style = "color:black">${
+            "$" +
+            price.toLocaleString("en-US", {
+                maximumFractionDigits: 2,
+            })
+        }</span>`;
+        //검은색
+    }
+    // prevPriceList의 가격정보를 불러와 가격을 비교한다
+    // -1인덱스가격 (최신가격)이 -2인덱스가격 (전가격)보다 높으면 초록색, 낮으면 빨간색, 같으면 검은색으로 표시한다
+};
 
-// const paginationRender = () => {
-//     //total result
-//     //page
-//     //pgsize
-//     //groupsize
-//     //totalPages
-//     const totalPages = Math.ceil(totalResult / pageSize);
-//     //pagegroup
-//     const pageGroup = Math.ceil(page / groupSize);
-//     //lastpage
-//     let lastPage = pageGroup * groupSize;
-//     if (lastPage > totalPages) {
-//         lastPage = totalPages;
-//     }
-//     console.log("last", totalPages);
-//     //firstpage
-//     const firstPage =
-//         lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
-//     let paginationHTML = "";
-//     if (page === firstPage) {
-//         ("");
-//     } else {
-//         paginationHTML += `<a class="page-link" onclick="moveToPage(${firstPage})" aria-label="Previous">
-//       <span aria-hidden="true">&laquo;</span>
-//   </a>`;
-//         paginationHTML += `<li class="page-item" onclick="moveToPage(${
-//             page - 1
-//         })"><a class="page-link">Previous</a></li>`;
-//     }
-//     for (let i = firstPage; i < lastPage; i++) {
-//         paginationHTML += `<li class="page-item ${
-//             i === page ? "active" : ""
-//         }"onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
-//     }
-//     if (page === lastPage) {
-//         ("");
-//     } else {
-//         paginationHTML += `<li class="page-item" onclick="moveToPage(${
-//             page + 1
-//         })"><a class="page-link">Next</a></li>`;
-//         paginationHTML += `<a class="page-link" onclick="moveToPage(${totalPages})" aria-label="Next">
-//       <span aria-hidden="true">&raquo;</span>
-//     </a>`;
-//     }
-//     document.querySelector(".pagination").innerHTML = paginationHTML;
-// };
+const paginationRender = () => {
+    //total result
+    //page
+    //pgsize
+    //groupsize
+    //totalPages
+    const totalPages = Math.ceil(totalResult / pageSize);
+    //pagegroup
+    const pageGroup = Math.ceil(page / groupSize);
+    //lastpage
+    let lastPage = pageGroup * groupSize;
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+    console.log("last", totalPages);
+    //firstpage
+    const firstPage =
+        lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+    let paginationHTML = "";
+    if (page === firstPage) {
+        ("");
+    } else {
+        paginationHTML += `<a class="page-link" onclick="moveToPage(${firstPage})" aria-label="Previous">
+      <span aria-hidden="true">&laquo;</span>
+  </a>`;
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${
+            page - 1
+        })"><a class="page-link">Previous</a></li>`;
+    }
+    for (let i = firstPage; i < lastPage; i++) {
+        paginationHTML += `<li class="page-item ${
+            i === page ? "active" : ""
+        }"onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    }
+    if (page === lastPage) {
+        ("");
+    } else {
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${
+            page + 1
+        })"><a class="page-link">Next</a></li>`;
+        paginationHTML += `<a class="page-link" onclick="moveToPage(${totalPages})" aria-label="Next">
+      <span aria-hidden="true">&raquo;</span>
+    </a>`;
+    }
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+};
 
-// const moveToPage = (pageNum) => {
-//     getData();
-//     page = pageNum;
-// };
+const moveToPage = (pageNum) => {
+    getData();
+    page = pageNum;
+};
 
-// function Main(delay) {
-//     getData();
-//     setInterval(() => {
-//         getData();
-//     }, delay);
-// }
-// //20초마다 코인 정보를 업데이트한다
-// Main(30000);
+function Main(delay) {
+    getData();
+    setInterval(() => {
+        getData();
+    }, delay);
+}
+//20초마다 코인 정보를 업데이트한다
+Main(30000);
 
-// // 가장 뜨거운 코인, 가장 차가운 코인 top3
-// const topCoinRender = () => {
-//     // hot top3
-//     const hotList = coinList.slice(0, 3);
-//     console.log("hotList: " + hotList);
-//     let hotHTML = "";
+// 가장 뜨거운 코인, 가장 차가운 코인 top3
+const topCoinRender = () => {
+    // hot top3
+    const hotList = coinList.slice(0, 3);
+    console.log("hotList: " + hotList);
+    let hotHTML = "";
 
-//     for (i = 0; i < hotList.length; i++) {
-//         hotHTML += `<div class="hot-list list">
-//       <div class="coin-left">
-//         <div class="coin-rank">${hotList.indexOf(hotList[i]) + 1}</div>
-//         <div class="coin-names">
-//           <div class="coin-name">${hotList[i]["name"]}</div>
-//           <div class="coin-symbol">${hotList[i]["symbol"]}</div>
-//         </div>
-//       </div>
-//       <div class="coin-24h">${
-//           hotList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
-//       }</div>
-//     </div>`;
-//     }
-//     document.getElementById("hot-container").innerHTML = hotHTML;
+    for (i = 0; i < hotList.length; i++) {
+        hotHTML += `<div class="hot-list list">
+      <div class="coin-left">
+        <div class="coin-rank">${hotList.indexOf(hotList[i]) + 1}</div>
+        <div class="coin-names">
+          <div class="coin-name">${hotList[i]["name"]}</div>
+          <div class="coin-symbol">${hotList[i]["symbol"]}</div>
+        </div>
+      </div>
+      <div class="coin-24h">${
+          hotList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
+      }</div>
+    </div>`;
+    }
+    document.getElementById("hot-container").innerHTML = hotHTML;
 
-//     // cold top3
-//     const coldList = coinList.slice(-3);
-//     console.log("coldList: " + coldList);
-//     let coldHTML = "";
+    // cold top3
+    const coldList = coinList.slice(-3);
+    console.log("coldList: " + coldList);
+    let coldHTML = "";
 
-//     for (i = 0; i < coldList.length; i++) {
-//         coldHTML += `<div class="hot-list list">
-//       <div class="coin-left">
-//         <div class="coin-rank">${coldList.indexOf(coldList[i]) + 1}</div>
-//         <div class="coin-names">
-//           <div class="coin-name">${coldList[i]["name"]}</div>
-//           <div class="coin-symbol">${coldList[i]["symbol"]}</div>
-//         </div>
-//       </div>
-//       <div class="coin-24h">${
-//           coldList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
-//       }</div>
-//     </div>`;
-//     }
-//     document.getElementById("cold-container").innerHTML = coldHTML;
-// };
+    for (i = 0; i < coldList.length; i++) {
+        coldHTML += `<div class="hot-list list">
+      <div class="coin-left">
+        <div class="coin-rank">${coldList.indexOf(coldList[i]) + 1}</div>
+        <div class="coin-names">
+          <div class="coin-name">${coldList[i]["name"]}</div>
+          <div class="coin-symbol">${coldList[i]["symbol"]}</div>
+        </div>
+      </div>
+      <div class="coin-24h">${
+          coldList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
+      }</div>
+    </div>`;
+    }
+    document.getElementById("cold-container").innerHTML = coldHTML;
+};
 
 const APIKEY = "80c825a1-4e89-4d63-b2db-81486a36d7f7";
 let coinListItems = []; // 코인 정보를 담을 배열
@@ -271,23 +272,28 @@ mobileInputBox.addEventListener("keydown", (ev) => {
     if (ev.key === "Enter") mobileSearch();
 });
 
+// 검색 기능
 async function search(event) {
+    // enter 이벤트시 검색
     if (event.key === "Enter") {
         let searchCoin = inputBox.value.toUpperCase();
 
         await getlist(searchCoin);
-        rendering();
+        searchRender();
     }
 }
 
+// mobile search
 async function mobileSearch(event) {
     let searchCoin = mobileInputBox.value.toUpperCase();
     await getlist(searchCoin);
-    rendering();
+    searchRender();
 }
 
+// 검색 버튼 이벤트
 async function clickSearch(event) {}
 
+// 키워드 검색
 const getlist = async (keyword) => {
     try {
         const url = new URL(
@@ -315,6 +321,7 @@ const getlist = async (keyword) => {
 //     console.log(coinList.map((e) => e.symbol));
 // })();
 
+// 탐욕 지수
 const fearGreed = async () => {
     let endTime = Math.floor(Date.now() / 1000);
     let startTime = endTime - 604800;
@@ -341,7 +348,7 @@ const fearGreedRender = async () => {
 };
 fearGreedRender();
 
-const rendering = () => {
+const searchRender = () => {
     document.querySelector(".highlights").style.display = "none";
     if (coinListItems.length <= 0) {
         document.querySelector(".search-page").style.display = "block";
