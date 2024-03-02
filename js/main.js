658685aef2f8e4d9af6a80aa8762cfd1eaea2c3b
jebi2420 <jebi2420@gmail.com>
2024-03-02 15:58:04 +0900

    [add] 뜨거운 코인 css 완료

--- js/main.js
+++ js/main.js
@@ -279,7 +279,6 @@ const topCoinRender = () => {
 
   for (i = 0; i < hotList.length; i++) {
     hotHTML += `
-    <div class="topChart">
       <div class="hot-list list">
         <div class="coin-left">
           <div class="coin-rank">${hotList.indexOf(hotList[i]) + 1}</div>
@@ -297,7 +296,6 @@ const topCoinRender = () => {
           hotList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
         }</div>
       </div>
-    </div>
     `;
   }
   document.getElementById("hotChart").innerHTML = hotHTML;
@@ -311,7 +309,6 @@ const topCoinRender = () => {
 
   for (i = 0; i < coldList.length; i++) {
     coldHTML += `
-    <div class="topChart">
       <div class="hot-list list">
         <div class="coin-left">
           <div class="coin-rank">${coldList.indexOf(coldList[i]) + 1}</div>
@@ -329,7 +326,6 @@ const topCoinRender = () => {
           coldList[i]["quote"].USD["percent_change_24h"].toFixed(2) + "%"
         }</div>
       </div>
-    </div>
     `;
   }
   document.getElementById("coldChart").innerHTML = coldHTML;
@@ -630,25 +626,25 @@ document.querySelector(".toggleSwitch").addEventListener(
 );
 //regionend TOGGLE
 
-// 슬라이드 기능
-// const swiper = document.querySelector(".slide-wrapper");
-// const bullets = document.querySelectorAll(".slide-dot");
+//슬라이드 기능
+const swiper = document.querySelector(".slide-wrapper");
+const bullets = document.querySelectorAll(".slide-dot");
 
-// let currentSlide = 0;
+let currentSlide = 0;
 
-// const showSlide = (slideIndex) => {
-//   const slideWidth = document.querySelector(".slide-content").offsetWidth;
-//   swiper.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
-//   currentSlide = slideIndex;
+const showSlide = (slideIndex) => {
+  const slideWidth = document.querySelector(".slide-content").offsetWidth;
+  swiper.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+  currentSlide = slideIndex;
 
-//   bullets.forEach((bullet, index) => {
-//     if (index === currentSlide) {
-//       bullet.classList.add("active");
-//     } else {
-//       bullet.classList.remove("active");
-//     }
-//   });
-// };
+  bullets.forEach((bullet, index) => {
+    if (index === currentSlide) {
+      bullet.classList.add("active");
+    } else {
+      bullet.classList.remove("active");
+    }
+  });
+};
 
 bullets.forEach((bullet, index) => {
   bullet.addEventListener("click", () => {
@@ -656,19 +652,19 @@ bullets.forEach((bullet, index) => {
   });
 });
 
-// // // 오토 슬라이드
-// const intervalDuration = 5000;
+// // 오토 슬라이드
+const intervalDuration = 5000;
 
-// // // 슬라이드 변경 함수
-// const autoSlide = () => {
-//   const nextSlide = (currentSlide + 1) % bullets.length;
-//   showSlide(nextSlide);
-// };
+// // 슬라이드 변경 함수
+const autoSlide = () => {
+  const nextSlide = (currentSlide + 1) % bullets.length;
+  showSlide(nextSlide);
+};
 
-// // // 자동 슬라이드 설정(
-// const autoSlideInterval = setInterval(autoSlide, intervalDuration);
+// // 자동 슬라이드 설정(
+const autoSlideInterval = setInterval(autoSlide, intervalDuration);
 
-// showSlide(0);
+showSlide(0);
 
 // 뉴스 슬라이드 기능
 const news_swiper = document.querySelector(".news_slide-wrapper");
